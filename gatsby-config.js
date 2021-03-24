0a15f25c6e3fb42a2738a8214d23cc81557bd42a
Timothy Millward <timothy.millward@predictiveblack.com>
2021-03-24 10:37:17 +0000

    butter knife updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,26 @@
+
 module.exports = {
     plugins: [
       // ...otherPlugins,
    
      `gatsby-plugin-layout`,
     
-      {
-        resolve: "gatsby-plugin-firebase",
-        options: {
-          credentials: {
-            apiKey: "AIzaSyBnQcZS2BjRrw3vNrouMvjwA4RPsJLOElM",
-            authDomain: "butterknifestore.firebaseapp.com",
-            databaseURL: "https://butterknifestore-default-rtdb.europe-west1.firebasedatabase.app",
-            projectId: "butterknifestore",
-            storageBucket: "butterknifestore.appspot.com",
-            messagingSenderId: "645660492184",
-            appId: "1:645660492184:web:11b97d85aae2e61ae36bed",
-            measurementId: "G-4D0DCRTV07"
+      // {
+      //   resolve: "gatsby-plugin-firebase",
+      //   options: {
+      //     credentials: {
+      //       apiKey: "AIzaSyBnQcZS2BjRrw3vNrouMvjwA4RPsJLOElM",
+      //       authDomain: "butterknifestore.firebaseapp.com",
+      //       databaseURL: "https://butterknifestore-default-rtdb.europe-west1.firebasedatabase.app",
+      //       projectId: "butterknifestore",
+      //       storageBucket: "butterknifestore.appspot.com",
+      //       messagingSenderId: "645660492184",
+      //       appId: "1:645660492184:web:11b97d85aae2e61ae36bed",
+      //       measurementId: "G-4D0DCRTV07"
 
-          }
-        }
-      },
+      //     }
+      //   }
+      // },
       {
         resolve: `gatsby-plugin-env-variables`,
         options: {
@@ -29,9 +30,42 @@ module.exports = {
             "FIREBASE_PROJECT_ID",
             "FIREBASE_STORAGE_BUCKET",
             "FIREBASE_MESSAGING_SENDER_ID",
-          "FIREBASE_APP_ID"]
+            "FIREBASE_APP_ID",
+            "CONTENTFUL_ACCESS_TOKEN"
+        ]
         },
       },
-    ],
-  }
+          {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              spaceId: process.env.CONTENTFUL_SPACE_ID,
+              // Learn about environment variables: https://gatsby.dev/env-vars
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+          },
+          {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              spaceId: process.env.CONTENTFUL_SPACE_ID,
+              // Learn about environment variables: https://gatsby.dev/env-vars
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+              host: `preview.contentful.com`,
+            },
+          },
+          module.exports = {
+            plugins: [
+              {
+                resolve: `gatsby-source-contentful`,
+                options: {
+                  spaceId: process.env.CONTENTFUL_SPACE_ID,
+                  // Learn about environment variables: https://gatsby.app/env-vars
+                  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+                  downloadLocal: true,
+                },
+              },
+            ],
+          }
+        ],
+      }
+
   console.log(`Database url is :${process.env.DATABASE_URL}`)

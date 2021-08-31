51fb5a305b51e2d434d304d6cd458701151efb9c
Sarah <silhouettetechnician@gmail.com>
2021-08-31 05:07:36 +0100

    merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import SEO from '../components/Seo'
+import SEO from '../components/Seo';
 import { Carousel } from 'react-responsive-carousel';
 import styled from '@emotion/styled';
 import { Link, navigate } from "gatsby";
 import Flex from '../styles/Flex'
 import { graphql } from "gatsby"
-import video from '../assets/MASKSANDSWEATSSAM.mp4'
+import ReactPlayer from 'react-player'
+import video from '../assets/MASKSANDSWEATSSAM.mp4';
 
 const VideoWrapper = styled.div`
   position: relative;
   height: unset;
 `
-
 const App = ({ data }) => {
+
   const Slider = styled(Carousel)`
     display: flex
     justify-content: center;
     align-items: center;
     width: 100%;
     background: unset;
-    margin: 0 auto;
+    margin: 0 auto; 
 `
-  // console.log(HomeVideo, 'cideo')
   const {
     contentfulBrand: { companyName: companyName, brandImages },
   } = data
